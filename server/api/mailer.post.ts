ae12174e04e50b4dd90711a43070f6f03c11564c
Funwi Che <funwiche.dev@gmail.com>
2024-09-23 08:19:40 +0100

    Create mailer.post.ts

---
+++ server/api/mailer.post.ts
@@ -0,0 +1,23 @@
+import nodemailer from "nodemailer";
+
+export default defineEventHandler(async (event) => {
+  try {
+    let { name, email, phone, location, message }: any = await readBody(event);
+    const { mailer, $app }: any = useRuntimeConfig();
+    const html = `
+    <div style="font-size:14px;margin-bottom:8px;"><strong>Full name:</strong> ${name}</div>
+    <div style="font-size:14px;margin-bottom:8px;"><strong>Email Address: </strong>${email}</div>
+    <div style="font-size:14px;margin-bottom:8px;"><strong>Phone Number:</strong> ${phone}</div>
+    <div style="font-size:14px;margin-bottom:8px;"><strong>Country / Region:</strong> ${location}</div>
+    <div style="font-size:14px;margin-bottom:8px;"><strong>Message: </strong>${message}`;
+    const subject = "Contact us";
+    const from = `${name} <${$app.email}>`;
+    await nodemailer
+      .createTransport(mailer)
+      .sendMail({ from, to: $app.email, replyTo: email, subject, html });
+    return ["Your message has been sent successfully!", null];
+  } catch (error: any) {
+    console.log(error.message);
+    return [null, error.message];
+  }
+});
